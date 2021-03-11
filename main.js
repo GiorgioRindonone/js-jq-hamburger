e6c2f6d83487b1eb070e4c03e354c236d537e0ce
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-11 18:04:12 +0100

    update_4

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-$(".header-right a").click(function() {
-  $(".hamburger-menu").css({
-    "display":"block"
-  });
+// $(".header-right a").click(function() {
+//   $(".hamburger-menu").css({
+//     "display":"block"
+//   });
+// });
+//
+// $(".hamburger-menu a").click(function() {
+//   $(".hamburger-menu").css({
+//     "display":"none"
+//   });
+// });
+var menuOn = $(".header-right a");
+var menuOff = $(".close");
+var menu = $(".hamburger-menu");
+var button ;
+
+function OnOff() {
+  if (menu.hasClass("active")) {
+    button = $(".close");
+    menu.removeClass("active")
+  } else {
+    button = $(".header-right a");
+    menu.addClass("active");
+  }
+};
+
+
+
+
+menuOn.click(function() {
+  menu.addClass("active");
 });
 
-$(".hamburger-menu a").click(function() {
-  $(".hamburger-menu").css({
-    "display":"none"
-  });
+menuOff.click(function() {
+  menu.removeClass("active");
 });
